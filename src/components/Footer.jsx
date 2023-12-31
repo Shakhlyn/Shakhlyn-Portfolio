2f6cb9a84fb86a8094b4c051ab05689924f46f2f
Shakhlyn <shakhlyn.sh.du@gmail.com>
2023-12-31 17:54:34 +0600

    feat: Add developer's web address to the name of the develper in the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import { developer_link } from "../constants";
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-deep-blue text-slate-100 text-xs py-3 w-full ">
+    <footer className="bg-deep-blue text-slate-100 text-xs py-3 w-full">
       <p className="w-3/4 mx-auto flex justify-center ">
-        &copy; {currentYear} | Designed and developed by Shakhlyn
+        &copy; {currentYear} | Designed and developed by <span>&nbsp;</span>
+        <a href={developer_link}>
+          <span className="hover:text-yellow">Shakhlyn</span>
+        </a>
       </p>
     </footer>
   );
