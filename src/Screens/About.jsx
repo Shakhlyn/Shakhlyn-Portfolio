71916aa690cfce85bc84bdb5bf86335bd6dd87c7
Shakhlyn <shakhlyn.sh.du@gmail.com>
2024-02-26 15:39:44 +0600

    update: Update the 'about me' information.

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -13,22 +13,23 @@ const About = () => {
         </p>
         <p className="pt-8">
           My journey began with HTML5, CSS3, and JavaScript. I further expanded
-          my skillset by mastering Bootstrap, Tailwind, Node.js, Express.js,
-          MongoDB, React, Redux Toolkit Query. My thirst for knowledge extends
-          beyond the MERN stack. I am also actively involved in version control
-          using Git and GitHub. In addition, I have learned Python and have
-          gained proficiency in Linux command lines. Equipped with this
-          comprehensive stack, I successfully worked on some projects.
+          my skillset by mastering Bootstrap, Tailwind CSS, Node JS, Express JS,
+          MongoDB,Mongoose,REST API, React, Redux Toolkit, RTK Query. My thirst
+          for knowledge extends beyond the MERN stack. I am also actively
+          involved in version control using Git and GitHub. In addition, I have
+          learned Python and have gained proficiency in Linux command lines.
+          Equipped with this comprehensive stack, I successfully worked on some
+          projects.
         </p>
         <p className="pt-8">
-          Currently, I'm actively honing my skills in Vitest and Redis,
+          Currently, I'm actively honing my skills in Redis and PostgreSQL,
           exploring their potential to enhance my development workflow and
           deliver high-quality software. Furthermore, I'm committed to
-          continuous learning and am eager to explore Django and PostgreSQL,
-          expanding my technical toolkit for creating cutting-edge applications.
-          My ultimate goal is to excel as a backend developer, contributing to
-          the creation of robust and scalable applications that make a positive
-          impact.
+          continuous learning and am eager to explore Django and Docker after
+          learning PostgreSQL, expanding my technical toolkit for creating
+          cutting-edge applications. My ultimate goal is to excel as a backend
+          developer, contributing to the creation of robust and scalable
+          applications that make a positive impact.
         </p>
       </div>
     </section>
--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const gitHubLink = "https://github.com/Shakhlyn";
 export const emailLink = "mailto:shakhlyn.sh.du@gmail.com";
 export const LinkedInLink = "https://www.linkedin.com/in/shakhlyn/";
 export const resumeLink =
-  "https://drive.google.com/file/d/1WkG_IpGpE4zc76fwPgIUZOM0hhIefTb8/view?usp=sharing";
+  "https://drive.google.com/file/d/1UYpeH_dld_HWqh4EULeGFM7pilXt1dLI/view?usp=sharing";
 
 export const projectBookNook =
   "https://github.com/Shakhlyn/Book-Nook__Backend-RESTful-API";
